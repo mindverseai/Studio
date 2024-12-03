57769bf41e12e92947c4e3d1d0817b4817032fd3
Jack <jack@mind-verse.de>
2024-12-03 16:09:50 +0100

    Test

--- api/controllers/common/errors.py
+++
@@ -1,6 +0,0 @@
-from werkzeug.exceptions import HTTPException
-
-
-class FilenameNotExistsError(HTTPException):
-    code = 400
-    description = "The specified filename does not exist."
--- api/core/llm_generator/output_parser/errors.py
+++
@@ -1,2 +0,0 @@
-class OutputParserError(Exception):
-    pass
