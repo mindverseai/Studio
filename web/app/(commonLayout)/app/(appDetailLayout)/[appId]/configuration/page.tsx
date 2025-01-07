dc580ff501806c61ef608af973c7af1c893f7fcd
jack-mind-verse <jack@mind-verse.de>
2025-01-07 12:47:02 +0100

    Revert "Fix"
    This reverts commit c7f5c634fbdc0d7e41bb3b15dba7f5e65d21713d.

--- web/app/(commonLayout)/app/(appDetailLayout)/[appId]/configuration/page.tsx
+++ web/app/(commonLayout)/app/(appDetailLayout)/[appId]/configuration/page.tsx
@@ -1,16 +1,7 @@
-'use client'
-import React, { useEffect } from 'react'
-import { useTranslation } from 'react-i18next'
+import React from 'react'
 import Configuration from '@/app/components/app/configuration'
 
-const IConfiguration = () => {
-  const { t } = useTranslation()
-
-  useEffect(() => {
-    if (typeof window !== 'undefined')
-      document.title = `${t('appDebug.orchestrate')} - Mindverse`
-  }, [t])
-
+const IConfiguration = async () => {
   return (
     <Configuration />
   )
