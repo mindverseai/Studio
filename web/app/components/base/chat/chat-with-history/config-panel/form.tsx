8a4c9c7858fdd152829467185320c74e93fe8dbf
jack-mind-verse <jack@mind-verse.de>
2025-01-20 17:35:05 +0100

    Test

--- web/app/components/base/chat/chat-with-history/config-panel/form.tsx
+++ web/app/components/base/chat/chat-with-history/config-panel/form.tsx
@@ -1,13 +1,13 @@
-import { useCallback } from 'react'
-import { useTranslation } from 'react-i18next'
-import { useChatWithHistoryContext } from '../context'
-import Input from './form-input'
-import { PortalSelect } from '@/app/components/base/select'
-import { InputVarType } from '@/app/components/workflow/types'
-import { FileUploaderInAttachmentWrapper } from '@/app/components/base/file-uploader'
+import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { useChatWithHistoryContext } from "../context";
+import Input from "./form-input";
+import { PortalSelect } from "@/app/components/base/select";
+import { InputVarType } from "@/app/components/workflow/types";
+import { FileUploaderInAttachmentWrapper } from "@/app/components/base/file-uploader";
 
 const Form = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   const {
     appParams,
     inputsForms,
@@ -15,48 +15,54 @@ const Form = () => {
     newConversationInputsRef,
     handleNewConversationInputsChange,
     isMobile,
-  } = useChatWithHistoryContext()
+  } = useChatWithHistoryContext();
 
-  const handleFormChange = useCallback((variable: string, value: any) => {
-    handleNewConversationInputsChange({
-      ...newConversationInputsRef.current,
-      [variable]: value,
-    })
-  }, [newConversationInputsRef, handleNewConversationInputsChange])
+  const handleFormChange = useCallback(
+    (variable: string, value: any) => {
+      handleNewConversationInputsChange({
+        ...newConversationInputsRef.current,
+        [variable]: value,
+      });
+    },
+    [newConversationInputsRef, handleNewConversationInputsChange]
+  );
 
   const renderField = (form: any) => {
-    const {
-      label,
-      required,
-      variable,
-      options,
-    } = form
+    const { label, required, variable, options } = form;
 
-    if (form.type === 'text-input' || form.type === 'paragraph') {
+    if (form.type === "text-input" || form.type === "paragraph") {
       return (
         <Input
           form={form}
           value={newConversationInputs[variable]}
           onChange={handleFormChange}
         />
-      )
+      );
     }
-    if (form.type === 'number') {
+    if (form.type === "number") {
       return (
         <input
+          id={variable}
+          name={variable}
           className="grow h-9 rounded-lg bg-gray-100 px-2.5 outline-none appearance-none"
           type="number"
-          value={newConversationInputs[variable] || ''}
-          onChange={e => handleFormChange(variable, e.target.value)}
-          placeholder={`${label}${!required ? `(${t('appDebug.variableTable.optional')})` : ''}`}
+          value={newConversationInputs[variable] || ""}
+          onChange={(e) => handleFormChange(variable, e.target.value)}
+          placeholder={`${label}${
+            !required ? `(${t("appDebug.variableTable.optional")})` : ""
+          }`}
         />
-      )
+      );
     }
     if (form.type === InputVarType.singleFile) {
       return (
         <FileUploaderInAttachmentWrapper
-          value={newConversationInputs[variable] ? [newConversationInputs[variable]] : []}
-          onChange={files => handleFormChange(variable, files[0])}
+          value={
+            newConversationInputs[variable]
+              ? [newConversationInputs[variable]]
+              : []
+          }
+          onChange={(files) => handleFormChange(variable, files[0])}
           fileConfig={{
             allowed_file_types: form.allowed_file_types,
             allowed_file_extensions: form.allowed_file_extensions,
@@ -65,13 +71,13 @@ const Form = () => {
             fileUploadConfig: (appParams as any).system_parameters,
           }}
         />
-      )
+      );
     }
     if (form.type === InputVarType.multiFiles) {
       return (
         <FileUploaderInAttachmentWrapper
           value={newConversationInputs[variable]}
-          onChange={files => handleFormChange(variable, files)}
+          onChange={(files) => handleFormChange(variable, files)}
           fileConfig={{
             allowed_file_types: form.allowed_file_types,
             allowed_file_extensions: form.allowed_file_extensions,
@@ -80,38 +86,46 @@ const Form = () => {
             fileUploadConfig: (appParams as any).system_parameters,
           }}
         />
-      )
+      );
     }
 
     return (
       <PortalSelect
-        popupClassName='w-[200px]'
+        popupClassName="w-[200px]"
         value={newConversationInputs[variable]}
-        items={options.map((option: string) => ({ value: option, name: option }))}
-        onSelect={item => handleFormChange(variable, item.value as string)}
-        placeholder={`${label}${!required ? `(${t('appDebug.variableTable.optional')})` : ''}`}
+        items={options.map((option: string) => ({
+          value: option,
+          name: option,
+        }))}
+        onSelect={(item) => handleFormChange(variable, item.value as string)}
+        placeholder={`${label}${
+          !required ? `(${t("appDebug.variableTable.optional")})` : ""
+        }`}
       />
-    )
-  }
+    );
+  };
 
-  if (!inputsForms.length)
-    return null
+  if (!inputsForms.length) return null;
 
   return (
-    <div className='mb-4 py-2'>
-      {
-        inputsForms.map(form => (
+    <div className="mb-4 py-2">
+      {inputsForms.map((form) => (
+        <div
+          key={form.variable}
+          className={`flex mb-3 last-of-type:mb-0 text-sm text-gray-900 ${
+            isMobile && "!flex-wrap"
+          }`}
+        >
           <div
-            key={form.variable}
-            className={`flex mb-3 last-of-type:mb-0 text-sm text-gray-900 ${isMobile && '!flex-wrap'}`}
+            className={`shrink-0 mr-2 py-2 w-[128px] ${isMobile && "!w-full"}`}
           >
-            <div className={`shrink-0 mr-2 py-2 w-[128px] ${isMobile && '!w-full'}`}>{form.label}</div>
-            {renderField(form)}
+            {form.label}
           </div>
-        ))
-      }
+          {renderField(form)}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
