b5aa30e7be723cd5201bde142fc6e81754d81197
Jack <jack@mind-verse.de>
2025-03-10 13:42:12 +0100

    Remove Dify

--- web/app/activate/page.tsx
+++ web/app/activate/page.tsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import Header from '../signin/_header'
-import style from '../signin/page.module.css'
-import ActivateForm from './activateForm'
-import cn from '@/utils/classnames'
+import React from "react";
+import Header from "../signin/_header";
+import style from "../signin/page.module.css";
+import ActivateForm from "./activateForm";
+import cn from "@/utils/classnames";
 
 const Activate = () => {
   return (
-    <div className={cn(
-      style.background,
-      'flex w-full min-h-screen',
-      'sm:p-4 lg:p-8',
-      'gap-x-20',
-      'justify-center lg:justify-start',
-    )}>
-      <div className={
-        cn(
-          'flex w-full flex-col bg-white shadow rounded-2xl shrink-0',
-          'space-between',
-        )
-      }>
+    <div
+      className={cn(
+        style.background,
+        "flex w-full min-h-screen",
+        "sm:p-4 lg:p-8",
+        "gap-x-20",
+        "justify-center lg:justify-start"
+      )}
+    >
+      <div
+        className={cn(
+          "flex w-full flex-col bg-white shadow rounded-2xl shrink-0",
+          "space-between"
+        )}
+      >
         <Header />
         <ActivateForm />
-        <div className='px-8 py-6 text-sm font-normal text-gray-500'>
-          © {new Date().getFullYear()} LangGenius, Inc. All rights reserved.
+        <div className="px-8 py-6 text-sm font-normal text-gray-500">
+          © {new Date().getFullYear()} Mindverse Studio. All rights reserved.
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Activate
+export default Activate;
--- web/app/components/base/chat/embedded-chatbot/index.tsx
+++ web/app/components/base/chat/embedded-chatbot/index.tsx
@@ -1,30 +1,24 @@
-import {
-  useEffect,
-  useState,
-} from 'react'
-import { useAsyncEffect } from 'ahooks'
-import { useTranslation } from 'react-i18next'
-import { RiLoopLeftLine } from '@remixicon/react'
-import {
-  EmbeddedChatbotContext,
-  useEmbeddedChatbotContext,
-} from './context'
-import { useEmbeddedChatbot } from './hooks'
-import { isDify } from './utils'
-import { useThemeContext } from './theme/theme-context'
-import cn from '@/utils/classnames'
-import { checkOrSetAccessToken } from '@/app/components/share/utils'
-import AppUnavailable from '@/app/components/base/app-unavailable'
-import useBreakpoints, { MediaType } from '@/hooks/use-breakpoints'
-import Loading from '@/app/components/base/loading'
-import LogoHeader from '@/app/components/base/logo/logo-embedded-chat-header'
-import Header from '@/app/components/base/chat/embedded-chatbot/header'
-import ConfigPanel from '@/app/components/base/chat/embedded-chatbot/config-panel'
-import ChatWrapper from '@/app/components/base/chat/embedded-chatbot/chat-wrapper'
-import Tooltip from '@/app/components/base/tooltip'
+import { useEffect, useState } from "react";
+import { useAsyncEffect } from "ahooks";
+import { useTranslation } from "react-i18next";
+import { RiLoopLeftLine } from "@remixicon/react";
+import { EmbeddedChatbotContext, useEmbeddedChatbotContext } from "./context";
+import { useEmbeddedChatbot } from "./hooks";
+import { isDify } from "./utils";
+import { useThemeContext } from "./theme/theme-context";
+import cn from "@/utils/classnames";
+import { checkOrSetAccessToken } from "@/app/components/share/utils";
+import AppUnavailable from "@/app/components/base/app-unavailable";
+import useBreakpoints, { MediaType } from "@/hooks/use-breakpoints";
+import Loading from "@/app/components/base/loading";
+import LogoHeader from "@/app/components/base/logo/logo-embedded-chat-header";
+import Header from "@/app/components/base/chat/embedded-chatbot/header";
+import ConfigPanel from "@/app/components/base/chat/embedded-chatbot/config-panel";
+import ChatWrapper from "@/app/components/base/chat/embedded-chatbot/chat-wrapper";
+import Tooltip from "@/app/components/base/tooltip";
 
 const Chatbot = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   const {
     isMobile,
     appInfoError,
@@ -35,63 +29,71 @@ const Chatbot = () => {
     appChatListDataLoading,
     handleNewConversation,
     themeBuilder,
-  } = useEmbeddedChatbotContext()
+  } = useEmbeddedChatbotContext();
 
-  const chatReady = (!showConfigPanelBeforeChat || !!appPrevChatList.length)
-  const customConfig = appData?.custom_config
-  const site = appData?.site
+  const chatReady = !showConfigPanelBeforeChat || !!appPrevChatList.length;
+  const customConfig = appData?.custom_config;
+  const site = appData?.site;
 
-  const difyIcon = <LogoHeader />
+  const mindverseIcon = <LogoHeader />;
 
   useEffect(() => {
-    themeBuilder?.buildTheme(site?.chat_color_theme, site?.chat_color_theme_inverted)
+    themeBuilder?.buildTheme(
+      site?.chat_color_theme,
+      site?.chat_color_theme_inverted
+    );
     if (site) {
-      if (customConfig)
-        document.title = `${site.title}`
-      else
-        document.title = `${site.title} - Powered by Mindverse`
+      if (customConfig) document.title = `${site.title}`;
+      else document.title = `${site.title} - Powered by Mindverse`;
     }
-  }, [site, customConfig, themeBuilder])
+  }, [site, customConfig, themeBuilder]);
 
   if (appInfoLoading) {
-    return (
-      <Loading type='app' />
-    )
+    return <Loading type="app" />;
   }
 
   if (appInfoError) {
-    return (
-      <AppUnavailable />
-    )
+    return <AppUnavailable />;
   }
   return (
     <div>
       <Header
         isMobile={isMobile}
-        title={site?.title || ''}
-        customerIcon={isDify() ? difyIcon : ''}
+        title={site?.title || ""}
+        customerIcon={isDify() ? mindverseIcon : ""}
         theme={themeBuilder?.theme}
         onCreateNewChat={handleNewConversation}
       />
-      <div className='flex bg-white overflow-hidden'>
-        <div className={cn('h-[100vh] grow flex flex-col overflow-y-auto', isMobile && '!h-[calc(100vh_-_3rem)]')}>
-          {showConfigPanelBeforeChat && !appChatListDataLoading && !appPrevChatList.length && (
-            <div className={cn('flex w-full items-center justify-center h-full tablet:px-4', isMobile && 'px-4')}>
-              <ConfigPanel />
-            </div>
+      <div className="flex bg-white overflow-hidden">
+        <div
+          className={cn(
+            "h-[100vh] grow flex flex-col overflow-y-auto",
+            isMobile && "!h-[calc(100vh_-_3rem)]"
           )}
-          {appChatListDataLoading && chatReady && (
-            <Loading type='app' />
-          )}
+        >
+          {showConfigPanelBeforeChat &&
+            !appChatListDataLoading &&
+            !appPrevChatList.length && (
+              <div
+                className={cn(
+                  "flex w-full items-center justify-center h-full tablet:px-4",
+                  isMobile && "px-4"
+                )}
+              >
+                <ConfigPanel />
+              </div>
+            )}
+          {appChatListDataLoading && chatReady && <Loading type="app" />}
           {chatReady && !appChatListDataLoading && (
-            <div className='relative h-full pt-8 mx-auto w-full max-w-[720px]'>
+            <div className="relative h-full pt-8 mx-auto w-full max-w-[720px]">
               {!isMobile && (
-                <div className='absolute top-2.5 right-3 z-20'>
-                  <Tooltip
-                    popupContent={t('share.chat.resetChat')}
-                  >
-                    <div className='p-1.5 bg-white border-[0.5px] border-gray-100 rounded-lg shadow-md cursor-pointer' onClick={handleNewConversation}>
-                      <RiLoopLeftLine className="h-4 w-4 text-gray-500"/>
+                <div className="absolute top-2.5 right-3 z-20">
+                  <Tooltip popupContent={t("share.chat.resetChat")}>
+                    <div
+                      className="p-1.5 bg-white border-[0.5px] border-gray-100 rounded-lg shadow-md cursor-pointer"
+                      onClick={handleNewConversation}
+                    >
+                      <RiLoopLeftLine className="h-4 w-4 text-gray-500" />
                     </div>
                   </Tooltip>
                 </div>
@@ -102,13 +104,13 @@ const Chatbot = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const EmbeddedChatbotWrapper = () => {
-  const media = useBreakpoints()
-  const isMobile = media === MediaType.mobile
-  const themeBuilder = useThemeContext()
+  const media = useBreakpoints();
+  const isMobile = media === MediaType.mobile;
+  const themeBuilder = useThemeContext();
 
   const {
     appInfoError,
@@ -136,71 +138,72 @@ const EmbeddedChatbotWrapper = () => {
     appId,
     handleFeedback,
     currentChatInstanceRef,
-  } = useEmbeddedChatbot()
+  } = useEmbeddedChatbot();
 
-  return <EmbeddedChatbotContext.Provider value={{
-    appInfoError,
-    appInfoLoading,
-    appData,
-    appParams,
-    appMeta,
-    appChatListDataLoading,
-    currentConversationId,
-    currentConversationItem,
-    appPrevChatList,
-    pinnedConversationList,
-    conversationList,
-    showConfigPanelBeforeChat,
-    newConversationInputs,
-    newConversationInputsRef,
-    handleNewConversationInputsChange,
-    inputsForms,
-    handleNewConversation,
-    handleStartChat,
-    handleChangeConversation,
-    handleNewConversationCompleted,
-    chatShouldReloadKey,
-    isMobile,
-    isInstalledApp,
-    appId,
-    handleFeedback,
-    currentChatInstanceRef,
-    themeBuilder,
-  }}>
-    <Chatbot />
-  </EmbeddedChatbotContext.Provider>
-}
+  return (
+    <EmbeddedChatbotContext.Provider
+      value={{
+        appInfoError,
+        appInfoLoading,
+        appData,
+        appParams,
+        appMeta,
+        appChatListDataLoading,
+        currentConversationId,
+        currentConversationItem,
+        appPrevChatList,
+        pinnedConversationList,
+        conversationList,
+        showConfigPanelBeforeChat,
+        newConversationInputs,
+        newConversationInputsRef,
+        handleNewConversationInputsChange,
+        inputsForms,
+        handleNewConversation,
+        handleStartChat,
+        handleChangeConversation,
+        handleNewConversationCompleted,
+        chatShouldReloadKey,
+        isMobile,
+        isInstalledApp,
+        appId,
+        handleFeedback,
+        currentChatInstanceRef,
+        themeBuilder,
+      }}
+    >
+      <Chatbot />
+    </EmbeddedChatbotContext.Provider>
+  );
+};
 
 const EmbeddedChatbot = () => {
-  const [initialized, setInitialized] = useState(false)
-  const [appUnavailable, setAppUnavailable] = useState<boolean>(false)
-  const [isUnknownReason, setIsUnknownReason] = useState<boolean>(false)
+  const [initialized, setInitialized] = useState(false);
+  const [appUnavailable, setAppUnavailable] = useState<boolean>(false);
+  const [isUnknownReason, setIsUnknownReason] = useState<boolean>(false);
 
   useAsyncEffect(async () => {
     if (!initialized) {
       try {
-        await checkOrSetAccessToken()
-      }
-      catch (e: any) {
+        await checkOrSetAccessToken();
+      } catch (e: any) {
         if (e.status === 404) {
-          setAppUnavailable(true)
-        }
-        else {
-          setIsUnknownReason(true)
-          setAppUnavailable(true)
+          setAppUnavailable(true);
+        } else {
+          setIsUnknownReason(true);
+          setAppUnavailable(true);
         }
       }
-      setInitialized(true)
+      setInitialized(true);
     }
-  }, [])
+  }, []);
 
-  if (!initialized)
-    return null
+  if (!initialized) return null;
 
   if (appUnavailable)
-    return <AppUnavailable isUnknownReason={isUnknownReason} />
+    return <AppUnavailable isUnknownReason={isUnknownReason} />;
 
-  return <EmbeddedChatbotWrapper />
-}
+  return <EmbeddedChatbotWrapper />;
+};
 
-export default EmbeddedChatbot
+export default EmbeddedChatbot;
